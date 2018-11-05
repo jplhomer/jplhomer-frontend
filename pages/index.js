f2d912c07bd4f2405a05ac5272ec56cbf1c6d2f3
Josh Larson <jplhomer@gmail.com>
2018-11-05 16:34:41 -0600

    Remove old things

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import App from '../components/App'
 import Header from '../components/Header'
-import Submit from '../components/Submit'
 import PostList from '../components/PostList'
 
 export default () => (
