d5090c0ecee584c62177ccaacd64d7c60a97100a
Josh Larson <jplhomer@gmail.com>
2018-11-06 10:26:45 -0600

    Add some phrases

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import { withRouter } from 'next/router'
 const Header = ({ router: { pathname } }) => (
   <header>
     <Link prefetch href='/'>
-      <a className={pathname === '/' ? 'is-active' : ''}>Home</a>
+      <a className={pathname === '/' ? 'is-active' : ''}>Josh Larson</a>
     </Link>
     <Link prefetch href='/about'>
       <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
-import App from '../components/App'
-import Header from '../components/Header'
-import PostList from '../components/PostList'
+import App from "../components/App";
+import Header from "../components/Header";
+import PostList from "../components/PostList";
+import styled from "styled-components";
+import phrases from "../lib/hero-phrases";
 
-export default () => (
+const Hero = styled.div`
+  font-size: 18vh;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-weight: 800;
+`;
+
+const Home = ({ phrase }) => (
   <App>
     <Header />
+    <Hero>{phrase}</Hero>
     <PostList />
   </App>
-)
+);
+
+Home.getInitialProps = () => {
+  return { phrase: randomPhrase() };
+};
+
+function randomPhrase() {
+  // if (process.browser) return "";
+  return phrases[Math.floor(Math.random() * phrases.length)];
+}
+
+export default Home;
